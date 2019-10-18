4a02f8b84bbaf7f59e109469c9130b64899bdde9
Clément Habinshuti <hello@habbes.xyz>
2019-10-19 01:57:16 +0300

    Add zmq library

--- node.cpp
+++ node.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "zmq.hpp"
 #include "lock.h"
 
 constexpr const int DEFAULT_LOOPS = 4;
+constexpr const int BASE_PORT = 7200;
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +14,7 @@ int main(int argc, char *argv[])
     const int num_nodes = atoi(argv[1]);
     const int node_id = atoi(argv[2]);
     const int num_loops = argc >= 4 ? atoi(argv[3]) : DEFAULT_LOOPS;
+    const int node_port = node_id + BASE_PORT;
 
     DistributedLock lock(num_nodes, node_id);
 
